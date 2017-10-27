4e49030286b56bee7558c86c7598e5d51d77ced2
meyearwood <margoyearwood@gmail.com>
2017-10-27 02:15:05 -0400

    Fixing assignment.

--- liri.js
+++ liri.js
@@ -1,151 +1,240 @@
-var keys = require('./keys.js');
-var request = require('request');
-var twitter = require('twitter');
-var spotify = require('spotify');
-var client = new twitter('keys.twitterKeys');
+// Dependencies
+var keys = require('./keys');
+var Twit = require('twit');
+var Spotify = require('node-spotify-api');
+var https = require('https');
 var fs = require('fs');
 
-var nodeArgv = process.argv;
-var command = process.argv[2];
-var x = "";
-for (var i = 3; i < nodeArgv.length; i++) {
-    if (i > 3 && i < nodeArgv.length) {
-        x = x + "+" + nodeArgv[i];
-    } else {
-        x = x + nodeArgv[i];
+// Initialize MyTwit
+var MyTwit = new Twit(keys);
+
+// Initialize Spotify
+var MySpotify = new Spotify({
+    id: 'b05fe7bfc7ca4f3789cd379ce8684d9c',
+    secret: '5808901556ff4d5ba1f7c969c45b7072',
+});
+
+// Command Types
+var COMMANDS = {
+    MY_TWEETS: 'my-tweets',
+    SPOTIFY_THIS_SONG: 'spotify-this-song',
+    MOVIE_THIS: 'movie-this',
+    DO_WHAT_IT_SAYS: 'do-what-it-says',
+};
+
+// Get command
+var commandType = process.argv[2];
+
+// Decide how to handle the request
+var request = function() {
+    // console.log('commandType: ', commandType);
+    var commandHandler = getCommandHandler(commandType);
+    commandHandler();
+};
+
+// Get Command Handler
+var getCommandHandler = function(commandType) {
+    // Decide which function to call based on command type
+    switch (commandType) {
+        case COMMANDS.MY_TWEETS:
+            return handleMyTweets;
+        case COMMANDS.SPOTIFY_THIS_SONG:
+            return handleSpotifyThisSong;
+        case COMMANDS.MOVIE_THIS:
+            return handleMovieThis;
+        case COMMANDS.DO_WHAT_IT_SAYS:
+            return handleDoWhatItSays;
+        default:
+
     }
 }
 
-switch (command) {
-    case "my-tweets":
-        showTweets();
-        break;
+// Handle My Tweets
+// This will show your last 20 tweets and when they were created
+var handleMyTweets = function() {
+    console.log('Showing my last 20 tweets...');
 
-    case "spotify-this-song":
-        if (x) {
-            spotifySong(x);
-        } else {
-            spotifySong("Just Got Paid");
-        }
-        break;
-
-    case "movie-this":
-        if (x) {
-            omdbData(x)
-        } else {
-            omdbData("Coming to America");
-        }
-        break;
-
-    case "do-what-it-says":
-        doThing();
-        break;
-
-    default:
-        console.log("{Enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
-        break;
-}
-
-function showTweets() {
-
-    var screenName = { screen_name: 'MeyLiri'};
-    client.get('statuses/user_timeline', screenName, function(error, tweets, response) {
-        if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
-                var date = tweets[i].created_at;
-                console.log("@MeyLiri: " + tweets[i].text + " Created At: " + date.substring(0, 19));
-                console.log("-----------------------");
-
-
-                fs.appendFile('log.txt', "@MeyLiri:" + tweets[i].text + " Created At: " + date.substring(0, 19));
-                fs.appendFile('log.txt', "-----------------------");
+    MyTwit.get('statuses/user_timeline', { user_id: '922633944041959424', count: 20 }, function(err, data, response) {
+        if (data && data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                // Create number for message
+                var tweetNum = i + 1;
+                // Format message to something like, "1. Message goes here"
+                var tweetMessage = tweetNum + '. ' + data[i].text;
+                // Print out message
+                console.log(tweetMessage);
+                logData(tweetMessage);
             }
-        } else {
-            console.log('Error');
         }
     });
-}
+};
 
-function spotifySong(song) {
-    spotify.search({
-        type: 'track',
-        query: song
-    }, function(error, data) {
-        if (!error) {
-            for (var i = 0; i < data.tracks.items.length; i++) {
-                var songData = data.tracks.items[i];
+// Handle Spotify This Song
+// This will show the following information about the song:
+// - Artist(s)
+// - The song's name
+// - A preview link of the song from Spotify
+// - The album that the song is from
+var handleSpotifyThisSong = function(paramSongName) {
+    // Default Song Name
+    var defaultSongName = 'The Sign';
+    var songName = process.argv[3] || paramSongName || defaultSongName;
+    console.log('Search Results for "' + songName + '"');
+    console.log('');
+    console.log('');
 
-                console.log("Artist: " + songData.artists[0].name);
-
-                console.log("Song: " + songData.name);
-
-                console.log("Preview URL: " + songData.preview_url);
-
-                console.log("Album: " + songData.album.name);
-                console.log("-----------------------");
-
-                fs.appendFile('log.txt', songData.artists[0].name);
-                fs.appendFile('log.txt', songData.name);
-                fs.appendFile('log.txt', songData.preview_url);
-                fs.appendFile('log.txt', songData.album.name);
-                fs.appendFile('log.txt', "-----------------------");
-            }
+    MySpotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
         } else {
-            console.log('Error occurred.');
-        }
-    });
-}
+            // Get the results from data or set to empty array.
+            var results = (data && data.tracks && data.tracks.items) || [];
+            // console.log('results: ', results);
 
-function omdbData(movie) {
-    var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+            for (var i = 0; i < results.length; i++) {
+                var result = results[i];
+                var resultSongName = result.name;
+                var resultSongPreviewLink = result.preview_url;
+                var resultSongArtistsArr = result.artists || [];
+                var resultSongAlbumName = result.album && result.album.name;
+                var artistsTempArr = [];
+                var resultSongArtists = 'N/A';
 
-    request(omdbURL, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var body = JSON.parse(body);
-
-                console.log("Title: " + body.Title);
-                console.log("Year:" + body.Year);
-                console.log("IMDB Rating: " + body.imdbRating);
-                console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-                console.log("Country: " + body.Country);
-                console.log("Language: " + body.Language);
-                console.log("Plot: " + body.Plot);
-                console.log("Actors: " + body.Actors);
-
-                console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
-
-                fs.appendFile('log.txt', "Title: " + body.Title);
-                fs.appendFile('log.txt', "Release Year: " + body.Year);
-                fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-                fs.appendFile('log.txt', "Country: " + body.Country);
-                fs.appendFile('log.txt', "Language: " + body.Language);
-                fs.appendFile('log.txt', "Plot: " + body.Plot);
-                fs.appendFile('log.txt', "Actors: " + body.Actors);
-                fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-                fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
-
-            } else {
-                console.log('Error')
-            }
-            if (movie === "Coming to America"); {
-                    console.log("-----------------------");
-                    console.log("This movie is one of the best comedy movies in the 80s ");
-                    console.log("It is considered a Classic");
-
-                    //adds text to log.txt
-                    fs.appendFile('log.txt', "-----------------------");
-                    fs.appendFile('log.txt', "This movie is one of the best comedy movies in the 80s");
-                    fs.appendFile('log.txt', "It is considered a Classic");
+                // Loop through the artist objects and push the
+                // artist name to the temp array
+                for (var count = 0; count < resultSongArtistsArr.length; count++) {
+                    var artist = resultSongArtistsArr[count];
+                    artistsTempArr.push(artist.name);
                 }
-            });
 
-    }
+                // Set the value for resultSongArtists
+                if (artistsTempArr.length) {
+                    resultSongArtists = artistsTempArr.join(', ');
+                }
 
-    function doThing() {
-        fs.readFile('random.txt', "utf8", function(error, data) {
-            var txt = data.split(',');
 
-            spotifySong(txt[1]);
+                console.log('Song Name: ' + resultSongName);
+                logData('Song Name: ' + resultSongName);
+                console.log('   • Album: ' + resultSongAlbumName);
+                logData('   • Album: ' + resultSongAlbumName);
+                console.log('   • Artists: ' + resultSongArtists);
+                logData('   • Artists: ' + resultSongArtists);
+                console.log('   • Preview Link: ' + resultSongPreviewLink);
+                logData('   • Preview Link: ' + resultSongPreviewLink);
+                console.log('');
+                logData('');
+                console.log('');
+                logData('');
+            }
+        }
+    });
+};
+
+// Handle Movie This
+// This will output the following information:
+// - Title of the movie.
+// - Year the movie came out.
+// - IMDB Rating of the movie.
+// - Rotten Tomatoes Rating of the movie.
+// - Country where the movie was produced.
+// - Language of the movie.
+// - Plot of the movie.
+// - Actors in the movie.
+var handleMovieThis = function(paramMovieName) {
+    // Default Movie Name
+    var defaultMovieName = 'Mr. Nobody';
+    var movieName = process.argv[3] || paramMovieName || defaultMovieName;
+    var apiURI = 'https://www.omdbapi.com/?apikey=40e9cece&t=' + movieName;
+    console.log('Search Results for "' + movieName + '"');
+    console.log('');
+    console.log('');
+
+    https.get(apiURI, function(resp) {
+        var data = '';
+        var respObj;
+
+        // A chunk of data has been received.
+        resp.on('data', function(chunk) {
+            data = data + chunk;
+            // can also be written as data += chunk;
         });
-    }
+
+        // Got the whole response now.
+        resp.on('end', function() {
+            respObj = JSON.parse(data);
+            var movieTitle = respObj.Title;
+            var movieYear = respObj.Year;
+            var movieIMDBRating = respObj.Ratings[0].Value;
+            var movieRotTomatoesRating = respObj.Ratings[1].Value;
+            var movieCountry = respObj.Country;
+            var movieLang = respObj.Language;
+            var moviePlot = respObj.Plot;
+            var movieActors = respObj.Actors;
+
+            console.log('Title: ' + movieTitle);
+            logData('Title: ' + movieTitle);
+            console.log('Year: ' + movieYear);
+            logData('Year: ' + movieYear);
+            console.log('IMDB Rating: ' + movieIMDBRating);
+            logData('IMDB Rating: ' + movieIMDBRating);
+            console.log('Rotten Tomatoes Rating: ' + movieRotTomatoesRating);
+            logData('Rotten Tomatoes Rating: ' + movieRotTomatoesRating);
+            console.log('Country: ' + movieCountry);
+            logData('Country: ' + movieCountry);
+            console.log('Language: ' + movieLang);
+            logData('Language: ' + movieLang);
+            console.log('Plot: ' + moviePlot);
+            logData('Plot: ' + moviePlot);
+            console.log('Actors: ' + movieActors);
+            logData('Actors: ' + movieActors);
+            console.log('');
+            logData('');
+            console.log('');
+            logData('');
+        });
+
+    }).on("error", function(err) {
+        console.log('Error: ' + err.message);
+    });
+};
+
+// Handle Do What It Says
+// This will take the text inside of the random.txt file and use it to
+// call one of LIRI's commands.
+var handleDoWhatItSays = function() {
+    console.log('handleDoWhatItSays was called...');
+
+    fs.readFile('random.txt', function(err, data) {
+        if (err) {
+            console.log('Error: ' + err);
+        } else {
+            docText = data.toString();
+            var params = docText.split(',');
+            var commandHandler = getCommandHandler(params[0]);
+            if (params.length > 1) {
+                // Convert string to array
+                var queryParam = params[1].split('');
+                // Remove Quotes
+                queryParam.pop();
+                queryParam.shift();
+                // Convert Array back to string
+                queryParam = queryParam.join('');
+                commandHandler(queryParam);
+            } else {
+                commandHandler();
+            }
+        }
+    });
+};
+
+// Bonus: Output the data of each command to log.txt file.
+var logData = function(data) {
+    fs.appendFile('log.txt', data, function(err) {
+        if (err) {
+            console.log('Error: ' + err);
+        }
+    });
+};
+
+// Kick off request
+request();
